4cb29ddad4c0506436bfd8e174b4df18650729c8
Jennifer Aguado <jenniferaguadowong@gmail.com>
2024-05-13 17:04:29 -0600

    ⭐ FEAT: Fisrt Ionic App: Vue

---
+++ photo-gallery/src/views/Tab2Page.vue
@@ -0,0 +1,37 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Photo Gallery 🖼️</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-fab vertical="bottom" horizontal="center" slot="fixed">
+    <ion-fab-button @click="takePhoto()">
+      <ion-icon :icon="camera"></ion-icon>
+    </ion-fab-button>
+  </ion-fab>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+//import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import { camera, trash, close } from 'ionicons/icons';
+import {
+  IonPage,
+  IonHeader,
+  IonFab,
+  IonFabButton,
+  IonIcon,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonImg,
+} from '@ionic/vue';
+</script>
+
+
