5b0d19333845f2bd377a6d3d6602777ea091a594
Miguel Chavez <158406484+MigueCha@users.noreply.github.com>
2024-05-14 16:08:42 +0000

    🌟cambio total

---
+++ src/views/Tab2.vue
@@ -0,0 +1,43 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Vue Camera App</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+
+      <ion-grid>
+        <ion-row>
+          <ion-col size="6" :key="photo" v-for="photo in photos">
+            <ion-img :src="photo.webviewPath" @click="showActionSheet(photo)"></ion-img>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
+
+      <ion-fab vertical="bottom" horizontal="center" slot="fixed">
+          <ion-fab-button @click="takePhoto()">
+            <ion-icon :icon="camera"></ion-icon>
+          </ion-fab-button>
+      </ion-fab>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { camera, trash, close } from 'ionicons/icons';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonFab, IonFabButton, IonGrid, IonRow, IonCol, IonImg } from '@ionic/vue';
+import { usePhotoGallery, Photo } from '@/composables/usePhotoGallery';
+
+export default  {
+  name: 'Tab2',
+  components: { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonFab, IonFabButton, IonGrid, IonRow, IonCol, IonImg },
+  setup() {
+    const { takePhoto, photos, showActionSheet } = usePhotoGallery();
+    return {
+      camera, trash, close, takePhoto, photos, showActionSheet 
+    }
+  }
+}
+
+</script>
